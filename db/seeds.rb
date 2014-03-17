c9f63693ddfb13b213494d4dfa677854b873b4dd
sowmyagondala <gsowmya72@gmail.com>
2014-03-17 22:49:43 +0530

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 i1 = Internet.create(name: "bsnl", speed: "100mbps", amount: "1500")
-i2 = Internet.create(name: "beam", speed: "1mbps", amount: "1500")
-i3 = Internet.create(name: "tata", speed: "500mbps", amount: "1500")
-i4 = Internet.create(name: "airtel", speed: "400mbps", amount: "1500")
+i2 = Internet.create(name: "beam", speed: "1mbps", amount: "2500")
+i3 = Internet.create(name: "tata", speed: "500mbps", amount: "4500")
+i4 = Internet.create(name: "airtel", speed: "400mbps", amount: "5500")
